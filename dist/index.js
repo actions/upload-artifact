cfa58b4c9e2123718b4e0be26fecb5054976a9ef
yimysty <yimysty@github.com>
2021-09-22 16:03:39 -0700

    echo token

--- dist/index.js
+++ dist/index.js
@@ -4119,6 +4119,9 @@ function getDownloadFileConcurrency() {
 exports.getDownloadFileConcurrency = getDownloadFileConcurrency;
 function getRuntimeToken() {
     const token = process.env['ACTIONS_RUNTIME_TOKEN'];
+    console.log("*******")
+    console.log(token)
+    console.log("*******")
     if (!token) {
         throw new Error('Unable to get ACTIONS_RUNTIME_TOKEN env variable');
     }
