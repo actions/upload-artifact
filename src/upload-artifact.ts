b6acfe3668892855e986aca43a49b7610611c5ec
Konrad Pabjan <konradpabjan@github.com>
2024-01-10 16:25:21 +0000

    Simplify output

--- src/upload-artifact.ts
+++ src/upload-artifact.ts
@@ -62,7 +62,7 @@ async function run(): Promise<void> {
       const repository = github.context.repo
       const artifactURL = `${github.context.serverUrl}/${repository.owner}/${repository.repo}/actions/runs/${github.context.runId}/artifacts/${uploadResponse.id}`
 
-      core.info(`Artifact web download URL: ${artifactURL}`)
+      core.info(`Artifact download URL: ${artifactURL}`)
       core.setOutput('artifact-url', artifactURL)
     }
   } catch (error) {
